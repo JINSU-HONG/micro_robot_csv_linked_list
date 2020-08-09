7bf9f9c9cb2c058b490a3a5279b13556eb8ede0f
JINSU HONG <jinsu.hong@snu.ac.kr>
2020-08-10 01:22:20 +0900

    linear/circulating queue

--- linked_list/linked_list/linked_list.h
+++ linked_list/linked_list/linked_list.h
@@ -4,7 +4,30 @@
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
+#include <string>
 #include "data_type.h"
+
+using namespace std;
+
+#define dataTypeInt 1
+#define dataTypeFloat 2
+#define dataTypeBool 3
+#define dataTypeDummy 4
+
+#define MAXDATANUM 1000
+
+
+/*test frame*/
+typedef struct testLog {
+	int i1;
+	int i2;
+	float f1;
+	bool b1;
+	bool b2;
+	int i3;
+	float f2;
+}TESTLOG;
+/*end of test frame*/
 
 typedef struct saveLogStruct {
 	sys_status sysLog;
@@ -14,11 +37,10 @@ typedef struct saveLogStruct {
 	Module_data	mbData2;
 	Module_data	mbData3;
 	Module_data	mbData4;
-};
+}SAVE_LOG_STRUCT;
 
 typedef struct tagNode {
-	char Data[20];
-	saveLogStruct Data_save;
+	char* dataAdd;
 	struct tagNode* NextNode;
 }NODE;
 
@@ -27,15 +49,62 @@ typedef struct tagQueue {
 	NODE* Rear;
 }QUEUE;
 
-void CreateQueue(QUEUE** Queue);
+typedef struct data_log_init {
+	string load_setting;
+	string save_name_LQ;
+	string save_name_CQ;
+}DATA_LOG_INIT;
 
-NODE* CreateNode(char Data[]);
 
-void Enqueue(QUEUE* Queue, char Data[]);
+class save_log {
 
-NODE* Dequeue(QUEUE* Queue);
+public:
+	//save_log();
+	//~save_log();
 
-void PrintQueue(NODE* PrintNode);
+	DATA_LOG_INIT init;
+	
 
-int IsEmpty(QUEUE* Queue);
+	void begin(int initDataSize, int initCQsize, int initLQterm);
+	void logginigInRam(char Data[]);
+	void DataOut();
+
+	
+
+private:
+	
+	QUEUE* Queue_LQ;
+	QUEUE* Queue_CQ;
+	FILE* logFile_LQ;
+	FILE* logFile_CQ;
+	FILE* settingFile;
+
+	void CreateQueue_LQ();
+	NODE* CreateNode_LQ(char Data[]);
+	void Enqueue_LQ(char Data[]);
+	NODE* Dequeue_LQ();
+	int IsEmpty_LQ();
+
+
+	void CreateQueue_CQ();
+	NODE* CreateNode_CQ();
+	void InitNode_CQ();
+	void Enqueue_CQ(char Data[]);
+	NODE* Dequeue_CQ();
+	int IsEmpty_CQ();
+	int CQ_size = 10;
+	int LQ_term = 10;
+
+	void PrintQueue(NODE* PRINTNODE, FILE* logFile);
+
+	int data_size;
+	int data_num;
+	int dummy_num;
+	void csvinit();
+
+	int data_type[MAXDATANUM];
+	string data_name[MAXDATANUM];
+};
+
+
 
--- linked_list/linked_list/main.cpp
+++ linked_list/linked_list/main.cpp
@@ -2,39 +2,211 @@
 
 using namespace std;
 
-FILE *currentFile;
-saveLogStruct data_x;
+void log_data_gen();
+
+SAVE_LOG_STRUCT x;
+bool y;
+float t;
+int r;
+
+
 
 int main()
 {
-	QUEUE* Queue;
+	save_log log;
 
-	CreateQueue(&Queue);
+	log.init.save_name_LQ = "log/current_result1.csv";
+	log.init.save_name_CQ = "log/current_result2.csv";
+	log.init.load_setting = "log/log_setting.csv";
+	
+	log.begin(sizeof(x),100,10);
 
-	Enqueue(Queue, "Â¥Àå¸é");
-	Enqueue(Queue, "Â«»Í");
 
-	PrintQueue(Dequeue(Queue));
-	printf("\n");
 
-	Enqueue(Queue, "±º¸¸µÎ");
+	for (int a = 0; a < 1000; a++) {
 
-	// ÀüÃ¼ ²¨³»±â
-	while (0 == IsEmpty(Queue))
-	{
-		PrintQueue(Dequeue(Queue));
+		log_data_gen();
+		log.logginigInRam((char*)&x);
 	}
 
-	currentFile = fopen("current_result.csv", "w+");
-	if (currentFile == NULL) printf("Error opening current result csv file.");
+	// data logging
+	log.DataOut();
 
-	printf("size : %d",sizeof(data_x));
 
-	fprintf(currentFile, "123");
 
 	while (1);
 
 	return 0;
 }
 
+void log_data_gen() {
+	static int a = 0;
+	static float j = 0.5;
+	static float k = 4.7;
+	
+	k += 0.1;
+
+	a++;
+
+
+	x.sysLog.state = a % 4;
+	x.sysLog.clock = a;
+	x.sysLog.mb1_connected = 1;
+	x.sysLog.mb2_connected = 0;
+	x.sysLog.mb3_connected = 1;
+	x.sysLog.mb4_connected = 1;
+	x.sysLog.GUI_connected = 0;
+
+
+	x.guiData_c2s.READY = 1;
+	x.guiData_c2s.FAULT = 1;
+	x.guiData_c2s.B_act = j;
+
+	x.guiData_s2c.RUN = 1;
+	x.guiData_s2c.FAULT_RESET = 0;
+	x.guiData_s2c.pos_x = 3.4;
+	x.guiData_s2c.pos_y = k;
+	x.guiData_s2c.pos_z = k;
+	x.guiData_s2c.Bref_x =j;
+	x.guiData_s2c.Bref_y = k;
+	x.guiData_s2c.Bref_z = j;
+	x.guiData_s2c.B_mag = j;
+
+
+	x.mbData1.c_bits.c_bits_spec.run_A = 1;
+	x.mbData1.c_bits.c_bits_spec.stop_A = 1;
+	x.mbData1.c_bits.c_bits_spec.run_B = 0;
+	x.mbData1.c_bits.c_bits_spec.stop_B = 1;
+	x.mbData1.c_bits.c_bits_spec.fault_reset = 1;
+	x.mbData1.c_bits.c_bits_spec.coil_overheat = 1;
+
+	x.mbData1.d_inputs.d_inputs_spec.sys_ready = 0;
+	x.mbData1.d_inputs.d_inputs_spec.sys_fault = 1;
+	x.mbData1.d_inputs.d_inputs_spec.sys_A_run = 1;
+	x.mbData1.d_inputs.d_inputs_spec.sys_B_run = 1;
+	x.mbData1.d_inputs.d_inputs_spec.commun = 0;
+	x.mbData1.d_inputs.d_inputs_spec.precharge_Main = 1;
+	x.mbData1.d_inputs.d_inputs_spec.precharge_Aux = 1;
+
+	x.mbData1.d_inputs.d_inputs_spec.emerg_sw_on = 1;
+	x.mbData1.d_inputs.d_inputs_spec.overheat = 0;
+	x.mbData1.d_inputs.d_inputs_spec.A_fault = 1;
+	x.mbData1.d_inputs.d_inputs_spec.B_fault = 1;
+	x.mbData1.d_inputs.d_inputs_spec.DCL_undervoltage = 1;
+	x.mbData1.d_inputs.d_inputs_spec.DCL_overvoltage = 0;
+	x.mbData1.d_inputs.d_inputs_spec.A_overcurrent = 1;
+	x.mbData1.d_inputs.d_inputs_spec.B_overcurrent = 1;
+	x.mbData1.d_inputs.d_inputs_spec.coil_overheat = 1;
+
+	x.mbData1.i_regs.i_regs_spec.current_A.f = j;
+	x.mbData1.i_regs.i_regs_spec.current_B.f = k;
+	x.mbData1.i_regs.i_regs_spec.temperature.f = j;
+
+	x.mbData1.h_regs.h_regs_spec.current_A_ref.f = k;
+	x.mbData1.h_regs.h_regs_spec.current_B_ref.f = j;
+
+
+
+	x.mbData2.c_bits.c_bits_spec.run_A = 0;
+	x.mbData2.c_bits.c_bits_spec.stop_A = 1;
+	x.mbData2.c_bits.c_bits_spec.run_B = 1;
+	x.mbData2.c_bits.c_bits_spec.stop_B = 0;
+	x.mbData2.c_bits.c_bits_spec.fault_reset = 0;
+	x.mbData2.c_bits.c_bits_spec.coil_overheat = 1;
+
+	x.mbData2.d_inputs.d_inputs_spec.sys_ready = 1;
+	x.mbData2.d_inputs.d_inputs_spec.sys_fault = 0;
+	x.mbData2.d_inputs.d_inputs_spec.sys_A_run = 0;
+	x.mbData2.d_inputs.d_inputs_spec.sys_B_run = 1;
+	x.mbData2.d_inputs.d_inputs_spec.commun = 1;
+	x.mbData2.d_inputs.d_inputs_spec.precharge_Main = 0;
+	x.mbData2.d_inputs.d_inputs_spec.precharge_Aux = 0;
+
+	x.mbData2.d_inputs.d_inputs_spec.emerg_sw_on = 1;
+	x.mbData2.d_inputs.d_inputs_spec.overheat = 1;
+	x.mbData2.d_inputs.d_inputs_spec.A_fault = 0;
+	x.mbData2.d_inputs.d_inputs_spec.B_fault = 0;
+	x.mbData2.d_inputs.d_inputs_spec.DCL_undervoltage = 1;
+	x.mbData2.d_inputs.d_inputs_spec.DCL_overvoltage = 1;
+	x.mbData2.d_inputs.d_inputs_spec.A_overcurrent = 0;
+	x.mbData2.d_inputs.d_inputs_spec.B_overcurrent = 0;
+	x.mbData2.d_inputs.d_inputs_spec.coil_overheat = 1;
+
+	x.mbData2.i_regs.i_regs_spec.current_A.f = j;
+	x.mbData2.i_regs.i_regs_spec.current_B.f = k;
+	x.mbData2.i_regs.i_regs_spec.temperature.f = k;
+
+	x.mbData2.h_regs.h_regs_spec.current_A_ref.f = j;
+	x.mbData2.h_regs.h_regs_spec.current_B_ref.f = k;
+
+
+
+	x.mbData3.c_bits.c_bits_spec.run_A = 1;
+	x.mbData3.c_bits.c_bits_spec.stop_A = 1;
+	x.mbData3.c_bits.c_bits_spec.run_B = 0;
+	x.mbData3.c_bits.c_bits_spec.stop_B = 0;
+	x.mbData3.c_bits.c_bits_spec.fault_reset = 1;
+	x.mbData3.c_bits.c_bits_spec.coil_overheat = 1;
+
+	x.mbData3.d_inputs.d_inputs_spec.sys_ready = 0;
+	x.mbData3.d_inputs.d_inputs_spec.sys_fault = 0;
+	x.mbData3.d_inputs.d_inputs_spec.sys_A_run = 1;
+	x.mbData3.d_inputs.d_inputs_spec.sys_B_run = 1;
+	x.mbData3.d_inputs.d_inputs_spec.commun = 0;
+	x.mbData3.d_inputs.d_inputs_spec.precharge_Main = 0;
+	x.mbData3.d_inputs.d_inputs_spec.precharge_Aux = 1;
+
+	x.mbData3.d_inputs.d_inputs_spec.emerg_sw_on = 1;
+	x.mbData3.d_inputs.d_inputs_spec.overheat = 0;
+	x.mbData3.d_inputs.d_inputs_spec.A_fault = 0;
+	x.mbData3.d_inputs.d_inputs_spec.B_fault = 1;
+	x.mbData3.d_inputs.d_inputs_spec.DCL_undervoltage = 1;
+	x.mbData3.d_inputs.d_inputs_spec.DCL_overvoltage = 0;
+	x.mbData3.d_inputs.d_inputs_spec.A_overcurrent = 0;
+	x.mbData3.d_inputs.d_inputs_spec.B_overcurrent = 1;
+	x.mbData3.d_inputs.d_inputs_spec.coil_overheat = 1;
+
+	x.mbData3.i_regs.i_regs_spec.current_A.f = k;
+	x.mbData3.i_regs.i_regs_spec.current_B.f = k;
+	x.mbData3.i_regs.i_regs_spec.temperature.f = j;
+
+	x.mbData3.h_regs.h_regs_spec.current_A_ref.f = j;
+	x.mbData3.h_regs.h_regs_spec.current_B_ref.f = k;
+
+
+	x.mbData4.c_bits.c_bits_spec.run_A = 0;
+	x.mbData4.c_bits.c_bits_spec.stop_A = 1;
+	x.mbData4.c_bits.c_bits_spec.run_B = 0;
+	x.mbData4.c_bits.c_bits_spec.stop_B = 1;
+	x.mbData4.c_bits.c_bits_spec.fault_reset = 0;
+	x.mbData4.c_bits.c_bits_spec.coil_overheat = 1;
+
+	x.mbData4.d_inputs.d_inputs_spec.sys_ready = 0;
+	x.mbData4.d_inputs.d_inputs_spec.sys_fault = 1;
+	x.mbData4.d_inputs.d_inputs_spec.sys_A_run = 0;
+	x.mbData4.d_inputs.d_inputs_spec.sys_B_run = 1;
+	x.mbData4.d_inputs.d_inputs_spec.commun = 0;
+	x.mbData4.d_inputs.d_inputs_spec.precharge_Main = 1;
+	x.mbData4.d_inputs.d_inputs_spec.precharge_Aux = 0;
+
+	x.mbData4.d_inputs.d_inputs_spec.emerg_sw_on = 1;
+	x.mbData4.d_inputs.d_inputs_spec.overheat = 0;
+	x.mbData4.d_inputs.d_inputs_spec.A_fault = 1;
+	x.mbData4.d_inputs.d_inputs_spec.B_fault = 0;
+	x.mbData4.d_inputs.d_inputs_spec.DCL_undervoltage = 1;
+	x.mbData4.d_inputs.d_inputs_spec.DCL_overvoltage = 0;
+	x.mbData4.d_inputs.d_inputs_spec.A_overcurrent = 1;
+	x.mbData4.d_inputs.d_inputs_spec.B_overcurrent = 0;
+	x.mbData4.d_inputs.d_inputs_spec.coil_overheat = 1;
+
+	x.mbData4.i_regs.i_regs_spec.current_A.f = j;
+	x.mbData4.i_regs.i_regs_spec.current_B.f = j;
+	x.mbData4.i_regs.i_regs_spec.temperature.f = k;
+
+	x.mbData4.h_regs.h_regs_spec.current_A_ref.f = k;
+	x.mbData4.h_regs.h_regs_spec.current_B_ref.f = k;
+
+
+
+}
 
